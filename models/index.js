6a326546b1dbbe537f17379c72ec7dc34ce994e4
huewilliams <4879sinwoo@naver.com>
2019-02-09 18:22:23 +0900

    CREATE: set db connect and relate

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+
+const db = {};
+
+const sequelize = new Sequelize(
+    'overhere', //db 이름
+    'root', // 이름
+    'espada8012!', // 비밀번호
+    {
+        'host': '127.0.0.1', // 사용할 호스트
+        'dialect': 'mysql', // 사용할 DB 종류
+        'operatorsAliases': false //deprecated 된 연산자 사용
+    }
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = require('./User')(sequelize, Sequelize);
+db.Friend = require('./Friend')(sequelize, Sequelize);
+db.Request = require('./Request')(sequelize, Sequelize);
+db.Promise = require('./Promise')(sequelize, Sequelize);
+db.Member = require('./Member')(sequelize, Sequelize);
+
+db.User.belongsToMany(db.Friend, {through: 'UserFriend'});
+db.Friend.belongsToMany(db.User, {through: 'UserFriend'});
+
+db.User.belongsToMany(db.Request, {through: 'UserRequest'});
+db.Request.belongsToMany(db.User, {through: 'UserRequest'});
+
+db.Promise.hasMany(db.Member, {foreignKey: 'promiseId', sourceKey: 'id'});
+db.Member.belongsTo(db.Promise, {foreignKey:  'promiseId', targetKey: 'id'});
+
+module.exports = db;
