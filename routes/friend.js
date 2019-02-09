cb6e8099e98b5cd38e00b8c399775cbecf38881a
huewilliams <4879sinwoo@naver.com>
2019-02-10 02:24:15 +0900

    CREATE: GET/friend/search/:username API

--- routes/friend.js
+++ routes/friend.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { verify } = require('../middlewares/util');
 
+const {Sequelize: { Op }} = require('../models');
 const User = require('../models').User;
 const Friend = require('../models').Friend;
 const Request = require('../models').Request;
@@ -18,6 +19,34 @@ router.get('/', async (req, res, next)=>{
                 user = await User.find({where: {id: auth}});
                 exist = await user.getFriends();
                 res.json(exist);
+            }
+        } catch (err) {
+            next(err);
+        }
+    }
+});
+
+// 친구 찾기 API (친구 안되어있는 사람만 검색됨)
+router.get('/search/:username', async (req, res, next)=>{
+    let token = req.get("token");
+    if(token) {
+        try {
+            let auth = await verify(token, 'jwt');
+            if(auth) {
+                console.log(auth);
+                user = await User.find({where: {id: auth}});
+                exist = await user.getFriends();
+                let arr = [];
+                for(let i=0; i<exist.length; i++)
+                    arr.push(exist[i].targetId);
+                console.log(arr);
+                let result = await User.findAll({
+                    where: {
+                        username: req.params.username,
+                        id: {[Op.notIn] : arr},
+                    },
+                });
+                res.json(result);
             }
         } catch (err) {
             next(err);
